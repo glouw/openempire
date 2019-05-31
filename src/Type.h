454bd4d0a8bece4e0ce9f03648389e45ea2e481e
Gustav Louw <airglouw@gmail.com>
2019-05-31 09:00:00 -0700

    generic unit type

---
+++ src/Type.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef enum
+{
+    TYPE_NONE = -1,
+    TYPE_VILLAGER,
+    TYPE_BERRY_BUSH,
+    TYPE_GOLD_MINE,
+    TYPE_STONE_MINE,
+    TYPE_TREE_CHOPPED_DOWN,
+    TYPE_FOREST_TREE,
+}
+Type;
