e472172a532e522a25d954211abd63dd8c0b106a
Gustav Louw <airglouw@gmail.com>
2019-06-28 16:43:55 -0700

    remove double check

--- src/Units.c
+++ src/Units.c
@@ -438,9 +438,7 @@ static void CalculateBoidStressors(const Units units, Unit* const unit, const Ma
         Point stressors = zero;
         for(int32_t j = 0; j < UTIL_LEN(point); j++)
             stressors = Point_Add(stressors, point[j]);
-        unit->stressors = (Point_Mag(stressors) < 100) ? zero : stressors;
-        if(Point_Mag(unit->stressors) < 500) // XXX. Whats a good threshold?
-            unit->stressors = zero;
+        unit->stressors = (Point_Mag(stressors) < 500) ? zero : stressors;
     }
 }
 
