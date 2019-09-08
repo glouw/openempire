96f50b9d247fdfaa643ebdfa5db0ed5d1ddfb935
Gustav Louw <airglouw@gmail.com>
2019-09-08 14:17:57 -0700

    some building stuff

--- src/Buildings.c
+++ src/Buildings.c
@@ -37,15 +37,26 @@ static bool OutOfBounds(const Buildings builds, const Point point)
         || point.y >= builds.rows;
 }
 
-static bool IsFreeSpace(const Buildings builds, const Point p)
+static bool IsFreeSpace(const Buildings builds, const Building build)
 {
-    return builds.reference[p.x + p.y * builds.cols] == NULL; // XXX. Iterate for building size. // XXX. Check map for unwalkable tiles.
+    for(int32_t y = 0; y < build.trait.dimensions.y; y++)
+    for(int32_t x = 0; x < build.trait.dimensions.x; x++)
+        if(builds.reference[x + y * builds.cols] != NULL)
+            return false;
+    return true;
+}
+
+static void Place(const Buildings builds, Building* const ref)
+{
+    for(int32_t y = 0; y < ref->trait.dimensions.y; y++)
+    for(int32_t x = 0; x < ref->trait.dimensions.x; x++)
+        builds.reference[x + y * builds.cols] = ref;
 }
 
 Buildings Buildings_Append(Buildings builds, const Building build)
 {
     if(!OutOfBounds(builds, build.cart)
-    && IsFreeSpace(builds, build.cart))
+    && IsFreeSpace(builds, build))
     {
         if(builds.count == builds.max)
         {
@@ -55,7 +66,8 @@ Buildings Buildings_Append(Buildings builds, const Building build)
             builds.build = temp;
         }
         builds.build[builds.count] = build;
-        builds.reference[build.cart.x + build.cart.y * builds.cols] = &builds.build[builds.count]; // XXX. Iterate for building size.
+        Building* const ref = &builds.build[builds.count];
+        Place(builds, ref);
         builds.count++;
     }
     return builds;
