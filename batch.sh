22f41d5edb7310aee2d374434121ff12ca9b1a97
Gustav Louw <airglouw@gmail.com>
2019-12-26 14:06:09 -0800

    batch remove comments

--- batch.sh
+++ batch.sh
@@ -1,28 +1,19 @@
-# BATCH SIM.
-# NUMBER A NUMBER OF USERS TO A LOCALHOST SERVER.
 main()
 {
     BIN=openempires
     XRES=1300
     YRES=720
     USERS=2
-
-    # START THE LOCALHOST SERVER. MARK DOWN ITS PID.
     ./$BIN --server --users $USERS &
     SERVER_PID=$!
-
-    # INSTANTIATE A NUMBER OF USER CLIENTS TO CONNECT TO THE LOCALHOST SERVER.
     for (( i = 0; i < $USERS; i++ ))
     do
-        # USER CLIENTS WILL HAVE DIFFERENT SCREEN RESOLUTIONS FOR TESTING AND VISIBILITY PURPOSES.
         D=20
         X=$(($XRES - $D * i))
         Y=$(($YRES - $D * i))
         ./$BIN --xres $X --yres $Y &
         sleep 0.1
     done
-
-    # WAIT FOR THE MOST RECENT USER CLIENT PID TO EXIT. KILL THE SERVER PID IMMEDIATELY AFTER.
     LAST_PID=$!
     wait $LAST_PID
     kill $SERVER_PID
