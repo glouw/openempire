8f0636f878e02d567d4f28b54ad4be3861ab58b3
Gustav Louw <airglouw@gmail.com>
2020-01-04 04:10:27 -0800

    batch cleanup

--- batch.sh
+++ batch.sh
@@ -1,22 +1,17 @@
-main()
-{
-    BIN=openempires
-    XRES=1300
-    YRES=720
-    USERS=2
-    ./$BIN --quiet --server --users $USERS &
-    SERVER_PID=$!
-    for (( i = 0; i < $USERS; i++ ))
-    do
-        D=20
-        X=$(($XRES - $D * i))
-        Y=$(($YRES - $D * i))
-        ./$BIN --xres $X --yres $Y &
-        sleep 0.1
-    done
-    LAST_PID=$!
-    wait $LAST_PID
-    kill $SERVER_PID
-}
-
-main
+BIN=openempires
+XRES=1300
+YRES=720
+USERS=2
+./$BIN --quiet --server --users $USERS &
+SERVER_PID=$!
+for (( i = 0; i < $USERS; i++ ))
+do
+    D=20
+    X=$(($XRES - $D * i))
+    Y=$(($YRES - $D * i))
+    ./$BIN --xres $X --yres $Y &
+    sleep 0.1
+done
+LAST_PID=$!
+wait $LAST_PID
+kill $SERVER_PID
