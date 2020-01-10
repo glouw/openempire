0ae3a3fcb66eaeb9aecc438086632494f33b256f
Gustav Louw <airglouw@gmail.com>
2020-01-10 10:29:36 -0800

    remove c99 heretic features

--- src/Color.c
+++ src/Color.c
@@ -1,27 +1,27 @@
 #include "Color.h"
 
-static const char* const strings[] = {
-    [COLOR_BLU ] = "Blue",
-    [COLOR_RED ] = "Red",
-    [COLOR_GRN ] = "Green",
-    [COLOR_YEL ] = "Yellow",
-    [COLOR_ORG ] = "Orange",
-    [COLOR_CYN ] = "Cyan",
-    [COLOR_PRP ] = "Purple",
-    [COLOR_GRY ] = "Grey",
-    [COLOR_GAIA] = "Gaia",
+static const char* strings[] = {
+    "Blue",
+    "Red",
+    "Green",
+    "Yellow",
+    "Orange",
+    "Cyan",
+    "Purple",
+    "Grey",
+    "Gaia",
 };
 
 static const uint32_t colors[] = {
-    [COLOR_BLU ] = 0x000000FF,
-    [COLOR_RED ] = 0x00FF0000,
-    [COLOR_GRN ] = 0x0000FF00,
-    [COLOR_YEL ] = 0x00FFFF00,
-    [COLOR_ORG ] = 0x00FFA500,
-    [COLOR_CYN ] = 0x0000FFFF,
-    [COLOR_PRP ] = 0x00800080,
-    [COLOR_GRY ] = 0x00D3D3D3,
-    [COLOR_GAIA] = 0x00000000,
+    0x000000FF,
+    0x00FF0000,
+    0x0000FF00,
+    0x00FFFF00,
+    0x00FFA500,
+    0x0000FFFF,
+    0x00800080,
+    0x00D3D3D3,
+    0x00000000,
 };
 
 const char* Color_ToString(const Color color)
