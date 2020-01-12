318b078953ea2a48de0fe615f01e793ebe61d68b
Gustav Louw <airglouw@gmail.com>
2020-01-12 14:13:55 -0800

    hello netsim

--- netsim.sh
+++ netsim.sh
@@ -1,5 +1,4 @@
 DEV=lo
 
 sudo tc qdisc del dev $DEV root netem
-sudo tc qdisc add dev $DEV root netem delay 50ms
-# sudo tc qdisc add dev $DEV root netem delay 50ms 10ms 25% loss 0.1% 25% duplicate 1% corrupt 0.1%
+sudo tc qdisc add dev $DEV root netem delay 50ms 10ms 25% loss 0.1% 25% duplicate 1% corrupt 0.1%
