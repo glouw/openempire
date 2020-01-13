fed9c6fb14edc2f2bdd85bcda9e86f0bd1582da9
Gustav Louw <airglouw@gmail.com>
2020-01-13 14:47:43 -0800

    some weird experimentation

--- netsim.sh
+++ netsim.sh
@@ -1,7 +1,7 @@
 DEV=lo
 
 LATENCY=20ms
-VARIANCE=3ms
+VARIANCE=5ms
 
 sudo tc qdisc del dev $DEV root netem
 sudo tc qdisc add dev $DEV root netem delay $LATENCY $VARIANCE 25% loss 0.1% 25% duplicate 1% corrupt 0.1%
