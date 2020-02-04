0cf4b84381a8100f7c23999ace872642bdf2971c
Gustav Louw <airglouw@gmail.com>
2020-02-04 13:39:44 -0800

    fix event

--- src/Event.h
+++ src/Event.h
@@ -4,32 +4,33 @@
 
 typedef struct
 {
-    bool mouse_l        : 1;
-    bool mouse_r        : 1;
-    bool mouse_ld       : 1;
-    bool mouse_lu       : 1;
-    bool mouse_rd       : 1;
-    bool mouse_ru       : 1;
-    bool key_left_shift : 1;
-    bool key_q          : 1;
-    bool key_w          : 1;
-    bool key_e          : 1;
-    bool key_r          : 1;
-    bool key_t          : 1;
-    bool key_a          : 1;
-    bool key_s          : 1;
-    bool key_d          : 1;
-    bool key_f          : 1;
-    bool key_g          : 1;
-    bool key_z          : 1;
-    bool key_x          : 1;
-    bool key_c          : 1;
-    bool key_v          : 1;
-    bool key_b          : 1;
-    bool key_1          : 1;
-    bool key_2          : 1;
-    bool key_3          : 1;
-    bool key_left_ctrl  : 1;
-    bool tab            : 1;
+    uint32_t
+        mouse_l        : 1,
+        mouse_r        : 1,
+        mouse_ld       : 1,
+        mouse_lu       : 1,
+        mouse_rd       : 1,
+        mouse_ru       : 1,
+        key_left_shift : 1,
+        key_q          : 1,
+        key_w          : 1,
+        key_e          : 1,
+        key_r          : 1,
+        key_t          : 1,
+        key_a          : 1,
+        key_s          : 1,
+        key_d          : 1,
+        key_f          : 1,
+        key_g          : 1,
+        key_z          : 1,
+        key_x          : 1,
+        key_c          : 1,
+        key_v          : 1,
+        key_b          : 1,
+        key_1          : 1,
+        key_2          : 1,
+        key_3          : 1,
+        key_left_ctrl  : 1,
+        tab            : 1;
 }
 Event;
