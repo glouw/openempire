c66b6a4be0cc514ca8c9e023a31bea0fa8e7bd5e
Gustav Louw <airglouw@gmail.com>
2020-02-24 15:42:05 -0800

    make restore a little easier to read

--- src/Restore.c
+++ src/Restore.c
@@ -2,31 +2,54 @@
 
 #include "Util.h"
 
-Restore Restore_Recv(const TCPsocket socket)
+#include <assert.h>
+
+#define READY (0xCAFEBABE)
+
+static uint8_t* RecvBytes(const TCPsocket socket)
+{
+    int32_t size_total = 0;
+    assert(UTIL_TCP_RECV(socket, &size_total) == 4);
+    const uint32_t ready = READY;
+    UTIL_TCP_SEND(socket, &ready);
+    uint8_t* const buffer = UTIL_ALLOC(uint8_t, size_total);
+    for(int32_t bytes = 0; bytes < size_total;)
+        bytes += SDLNet_TCP_Recv(socket, &buffer[bytes], size_total);
+    return buffer;
+}
+
+static Restore Pack(uint8_t* const buffer)
 {
     static Restore zero;
     Restore restore = zero;
-    if(socket)
+    const int32_t* const count  = (int32_t*) &buffer[0];
+    const int32_t* const cycles = (int32_t*) &buffer[4];
+    const Share  * const stamp  = (Share  *) &buffer[8];
+    const Unit   * const unit   = (Unit   *) &buffer[8 + sizeof(restore.stamp)];
+    restore.count = *count;
+    if(restore.count > 0)
     {
-        int32_t header_size = 0;
-        UTIL_TCP_RECV(socket, &header_size);
-        uint8_t* const buffer = UTIL_ALLOC(uint8_t, header_size);
-        for(int32_t bytes = 0; bytes < header_size;)
-            bytes += SDLNet_TCP_Recv(socket, &buffer[bytes], header_size);
-        const int32_t* const count  = (int32_t*) &buffer[0];
-        const int32_t* const cycles = (int32_t*) &buffer[4];
-        const Share  * const stamp  = (Share  *) &buffer[8];
-        const Unit   * const unit   = (Unit   *) &buffer[8 + sizeof(restore.stamp)];
-        restore.count = *count;
-        restore.cycles = *cycles;
         restore.unit = UTIL_ALLOC(Unit, restore.count);
-        for(int32_t i = 0; i < COLOR_COUNT; i++)
-            restore.stamp[i] = stamp[i];
         for(int32_t i = 0; i < restore.count; i++)
             restore.unit[i] = unit[i];
-        free(buffer);
     }
+    restore.cycles = *cycles;
+    for(int32_t i = 0; i < COLOR_COUNT; i++)
+        restore.stamp[i] = stamp[i];
     return restore;
+}
+
+Restore Restore_Recv(const TCPsocket socket)
+{
+    if(socket)
+    {
+        uint8_t* const buffer = RecvBytes(socket);
+        const Restore restore = Pack(buffer);
+        free(buffer);
+        return restore;
+    }
+    static Restore zero;
+    return zero;
 }
 
 void Restore_Send(const Restore restore, const TCPsocket socket)
@@ -34,12 +57,15 @@ void Restore_Send(const Restore restore, const TCPsocket socket)
     if(socket)
     {
         const int32_t size = restore.count * sizeof(*restore.unit);
-        const int32_t header_size =
+        const int32_t size_total =
             sizeof(restore.count)
           + sizeof(restore.cycles)
           + size
           + sizeof(restore.stamp);
-        UTIL_TCP_SEND(socket, &header_size);
+        UTIL_TCP_SEND(socket, &size_total);
+        uint32_t ready = 0;
+        UTIL_TCP_RECV(socket, &ready);
+        assert(ready == READY);
         UTIL_TCP_SEND(socket, &restore.count);
         UTIL_TCP_SEND(socket, &restore.cycles);
         UTIL_TCP_SEND(socket, &restore.stamp);
