9889d933be24dfae96289f9c69b5cfb19cb8432d
Gustav Louw <airglouw@gmail.com>
2020-02-25 05:29:55 -0800

    include max size in stream buffer

--- src/Restore.c
+++ src/Restore.c
@@ -1,31 +1,34 @@
 #include "Restore.h"
 
 #include "Util.h"
+#include "Config.h"
 
-#include <assert.h>
-
-#define READY (0xCAFEBABE)
-
-static uint8_t* RecvBytes(const TCPsocket socket)
+static int32_t GetRestoreHeaderByteSize(const Restore restore)
 {
-    int32_t size_total = 0;
-    assert(UTIL_TCP_RECV(socket, &size_total) == 4);
-    const uint32_t ready = READY;
-    UTIL_TCP_SEND(socket, &ready);
-    uint8_t* const buffer = UTIL_ALLOC(uint8_t, size_total);
-    for(int32_t bytes = 0; bytes < size_total;)
-        bytes += SDLNet_TCP_Recv(socket, &buffer[bytes], size_total);
-    return buffer;
+    return sizeof(restore.count)
+         + sizeof(restore.cycles)
+         + sizeof(restore.stamp);
 }
 
-static Restore Pack(uint8_t* const buffer)
+static Restore RecvPacked(const TCPsocket socket)
 {
     static Restore zero;
     Restore restore = zero;
-    const int32_t* const count  = (int32_t*) &buffer[0];
-    const int32_t* const cycles = (int32_t*) &buffer[4];
-    const Share  * const stamp  = (Share  *) &buffer[8];
-    const Unit   * const unit   = (Unit   *) &buffer[8 + sizeof(restore.stamp)];
+    const int32_t size = CONFIG_UNITS_MAX * sizeof(*restore.unit);
+    const int32_t size_header = GetRestoreHeaderByteSize(restore);
+    const int32_t size_max = size_header + size;
+    uint8_t* const buffer = UTIL_ALLOC(uint8_t, size_max);
+    const int32_t* const size_real = (int32_t*) &buffer[ 0]; // THE REAL SIZE OF THE INCOMING STREAM
+    const int32_t* const count     = (int32_t*) &buffer[ 4]; // WAS CHOSEN TO BE INCLUDED IN THE STREAM.
+    const int32_t* const cycles    = (int32_t*) &buffer[ 8]; // IT IS CHICKEN AND EGG, BUT IT IS MORE RELIABLE.
+    const Share  * const stamp     = (Share  *) &buffer[12]; // THAN SPLITTING INTO 2 RECV CALLS.
+    const Unit   * const unit      = (Unit   *) &buffer[12 + sizeof(restore.stamp)];
+    for(int32_t bytes = 0; bytes < size_max;)
+    {
+        bytes += SDLNet_TCP_Recv(socket, &buffer[bytes], size_max);
+        if(bytes >= *size_real)
+            break;
+    }
     restore.count = *count;
     if(restore.count > 0)
     {
@@ -36,20 +39,16 @@ static Restore Pack(uint8_t* const buffer)
     restore.cycles = *cycles;
     for(int32_t i = 0; i < COLOR_COUNT; i++)
         restore.stamp[i] = stamp[i];
+    free(buffer);
     return restore;
 }
 
 Restore Restore_Recv(const TCPsocket socket)
 {
-    if(socket)
-    {
-        uint8_t* const buffer = RecvBytes(socket);
-        const Restore restore = Pack(buffer);
-        free(buffer);
-        return restore;
-    }
     static Restore zero;
-    return zero;
+    return socket
+        ? RecvPacked(socket)
+        : zero;
 }
 
 void Restore_Send(const Restore restore, const TCPsocket socket)
@@ -57,15 +56,9 @@ void Restore_Send(const Restore restore, const TCPsocket socket)
     if(socket)
     {
         const int32_t size = restore.count * sizeof(*restore.unit);
-        const int32_t size_total =
-            sizeof(restore.count)
-          + sizeof(restore.cycles)
-          + size
-          + sizeof(restore.stamp);
+        const int32_t size_header = GetRestoreHeaderByteSize(restore);
+        const int32_t size_total = size_header + size;
         UTIL_TCP_SEND(socket, &size_total);
-        uint32_t ready = 0;
-        UTIL_TCP_RECV(socket, &ready);
-        assert(ready == READY);
         UTIL_TCP_SEND(socket, &restore.count);
         UTIL_TCP_SEND(socket, &restore.cycles);
         UTIL_TCP_SEND(socket, &restore.stamp);
