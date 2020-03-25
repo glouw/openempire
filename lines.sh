76face733541861e25bb0e824df0d76c86abb7ed
Gustav Louw <airglouw@gmail.com>
2020-03-25 07:26:46 -0700

    lines tool

---
+++ lines.sh
@@ -0,0 +1,11 @@
+# COUNTS THE NUMBER OF LINES THE C COMPILER MUST
+# ULTIMATELY CHEW THROUGH AFTER THE PREPROCESSOR IS INVOKED
+
+TOTAL=0
+for FILE in ./src/*.c;
+do
+    echo "counting $FILE"
+    LINES=$(gcc -E $FILE | wc --lines)
+    TOTAL=$(( $TOTAL + $LINES ))
+done
+echo "$TOTAL lines of C to chew through _AFTER_ the C PREPROCESSOR"
