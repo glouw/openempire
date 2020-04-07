12909ef7d0b581512c04f01e874e3922ad5f50b9
Gustav Louw <airglouw@gmail.com>
2020-04-07 10:24:14 -0700

    better restore

--- src/Restore.c
+++ src/Restore.c
@@ -7,33 +7,16 @@
 
 #define RESTORE_COMPLETE (0xAB)
 
-static int32_t GetRestoreHeaderByteSize(const Restore restore)
-{
-    return sizeof(int32_t) // REAL SIZE (size_real) OF WHOLE STREAM IS INCLUDED.
-         + sizeof(restore.count)
-         + sizeof(restore.cycles)
-         + sizeof(restore.command_group_next)
-         + sizeof(restore.id_next)
-         + sizeof(restore.share);
-}
+#define HEADER_SIZE (sizeof(int32_t) + sizeof(Restore)) // REAL SIZE (size_real) OF WHOLE STREAM IS INCLUDED.
 
 static Restore RecvPacked(const TCPsocket socket)
 {
-    static Restore zero;
-    Restore restore = zero;
-    const int32_t size_units = CONFIG_UNITS_MAX * sizeof(*restore.unit);
-    const int32_t size_header = GetRestoreHeaderByteSize(restore);
-    const int32_t size_max = size_header + size_units;
+    const int32_t size_units = CONFIG_UNITS_MAX * sizeof(Unit);
+    const int32_t size_max = HEADER_SIZE + size_units;
     uint8_t* const buffer = UTIL_ALLOC(uint8_t, size_max);
-    /* ---------- HEADER --------- */
-    const int32_t* const size_real          = (int32_t*) &buffer[ 0]; // THE REAL SIZE OF THE INCOMING STREAM
-    const int32_t* const count              = (int32_t*) &buffer[ 4]; // WAS CHOSEN TO BE INCLUDED IN THE STREAM.
-    const int32_t* const cycles             = (int32_t*) &buffer[ 8]; // IT IS CHICKEN AND EGG, BUT IT IS MORE RELIABLE.
-    const int32_t* const id_next            = (int32_t*) &buffer[12]; // THAN SPLITTING THE RECIEVER INTO TWO RECV CALLS.
-    const int32_t* const command_group_next = (int32_t*) &buffer[16];
-    const Share  * const share              = (Share  *) &buffer[20];
-    /* ---------- UNITS ---------- */
-    const Unit * const unit = (Unit*) &buffer[20 + sizeof(restore.share)];
+    const int32_t* const size_real = (int32_t*) &buffer[0];
+    const Restore* const restore   = (Restore*) &buffer[4];
+    const Unit*    const unit      = (Unit*)    &buffer[4 + sizeof(Restore)];
     int32_t bytes = 0;
     while(bytes < size_max)
     {
@@ -42,22 +25,17 @@ static Restore RecvPacked(const TCPsocket socket)
             break;
     }
     assert(bytes == *size_real);
-    restore.count = *count;
-    if(restore.count > 0)
+    Restore out = *restore;
+    if(out.count > 0)
     {
-        restore.unit = UTIL_ALLOC(Unit, restore.count);
-        for(int32_t i = 0; i < restore.count; i++)
-            restore.unit[i] = unit[i];
+        out.unit = UTIL_ALLOC(Unit, out.count);
+        for(int32_t i = 0; i < out.count; i++)
+            out.unit[i] = unit[i];
     }
-    restore.cycles = *cycles;
-    restore.id_next = *id_next;
-    restore.command_group_next = *command_group_next;
-    for(int32_t i = 0; i < COLOR_COUNT; i++)
-        restore.share[i] = share[i];
     free(buffer);
     const uint8_t ack = RESTORE_COMPLETE;
     UTIL_TCP_SEND(socket, &ack);
-    return restore;
+    return out;
 }
 
 Restore Restore_Recv(const TCPsocket socket)
@@ -70,19 +48,11 @@ Restore Restore_Recv(const TCPsocket socket)
 
 void Restore_Send(const Restore restore, const TCPsocket socket)
 {
-    const int32_t size_header = GetRestoreHeaderByteSize(restore);
     const int32_t size_units = restore.count * sizeof(*restore.unit);
-    const int32_t size_real = size_header + size_units;
-    /* ---------- HEADER --------- */
+    const int32_t size_real = HEADER_SIZE + size_units;
     UTIL_TCP_SEND(socket, &size_real);
-    UTIL_TCP_SEND(socket, &restore.count);
-    UTIL_TCP_SEND(socket, &restore.cycles);
-    UTIL_TCP_SEND(socket, &restore.id_next);
-    UTIL_TCP_SEND(socket, &restore.command_group_next);
-    UTIL_TCP_SEND(socket, &restore.share);
-    /* ---------- UNITS ---------- */
+    UTIL_TCP_SEND(socket, &restore);
     SDLNet_TCP_Send(socket, restore.unit, size_units);
-    /* ----- ASSERT COMPLETE ----- */
     uint8_t ack = 0;
     UTIL_TCP_RECV(socket, &ack);
     assert(ack == RESTORE_COMPLETE);
